4ea2b9e889f9bdfa6741984d9bf2dd3b61e10850
Tikendra Sahu <68785366+tikendraw@users.noreply.github.com>
2022-10-14 19:47:14 +0530

    updated format

--- preprocessing/image.py
+++ preprocessing/image.py
@@ -15,6 +15,10 @@ def resize_func( file_path:str, dest_path:str = None ,px:int  = 512, keep_aspect
                         resized files will be kept in file_path)
                         
         px:int = max pixel range to resize upto
+
+        keep_aspect_ratio =  keep aspect ratio ?
+
+        format:str = default: None ['PNG','JPEG'] any one
         
         quality:int = quality range between 1 to 100  
     '''
@@ -23,17 +27,19 @@ def resize_func( file_path:str, dest_path:str = None ,px:int  = 512, keep_aspect
         dest_path = file_path
     
     for item in tqdm(os.listdir(file_path)):
-        img_path = os.path.join(file_path, item)
-        img_name = item.split('.')[:-1]
-        
+        img_path = os.path.join(file_path, item)        
         img = Image.open(img_path)
-
-        # print(img_path)
+        #Dimensions
         w, h = img. size
         print('height:', h, 'width: ', w)
-
+        #ratio
         ratio = h/w
         print(ratio)
+
+        filename, extension = os.path.splitext(item)
+        print(filename, '   :  ', extension)
+
+
         if keep_aspect_ratio:        
             if ratio < 1:
                 h = px * ratio
@@ -53,7 +59,8 @@ def resize_func( file_path:str, dest_path:str = None ,px:int  = 512, keep_aspect
         print('new height:', h, 'new width:',w)
 
         img_resized = img.resize((int(w),int(h)), Image.ANTIALIAS)
-        img_resized.save(dest_path + f'/{img_name}_resized',format=format, quality=100)
+        img_resized.save(dest_path + f'/{filename}_resized{extension}', format = format, quality=100)
+
 
 
 
