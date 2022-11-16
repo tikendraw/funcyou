648d8e5c86582884d64fdd4e804c9a942fb16518
Tikendra Sahu <68785366+tikendraw@users.noreply.github.com>
2022-11-16 12:26:37 +0530

    added tensorboard callback

--- callbacks.py
+++ callbacks.py
@@ -29,6 +29,21 @@ def create_model_checkpoint(model_name, save_dir, monitor:str = 'val_loss',verbo
 
 
 
+def create_tensorboard_callback(dir_name, experiment_name):
+	"""
+	Creates a TensorBoard callback instand to store log files.
+	Stores log files with the filepath:
+	"dir_name/experiment_name/current_datetime/"
+	Args:
+	dir_name: target directory to store TensorBoard log files
+	experiment_name: name of experiment directory (e.g. efficientnet_model_1)
+	"""
+	log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+	tensorboard_callback = tf.keras.callbacks.TensorBoard(
+	  log_dir=log_dir
+	)
+	print(f"Saving TensorBoard log files to: {log_dir}")
+	return tensorboard_callback
 
 
 
