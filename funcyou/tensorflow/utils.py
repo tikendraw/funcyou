5020a7f309eab3600498d62269d5599b9c2d086c
Tikendra Sahu <68785366+tikendraw@users.noreply.github.com>
2023-07-17 07:39:07 +0530

    create bert instance

--- funcyou/tensorflow/utils.py
+++ funcyou/tensorflow/utils.py
@@ -64,7 +64,19 @@ def plot_history(history, plot = ['loss','accuracy'], split = ['train','val'], e
         print('Error Occured: ',e)
 
 		
-		
+def create_bert(trainable=False):
+    text_input = tf.keras.layers.Input(shape=(), dtype=tf.string)
+    preprocessor = hub.KerasLayer(
+        "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3")
+    encoder_inputs = preprocessor(text_input)
+    encoder = hub.KerasLayer(
+        "https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-12_H-128_A-2/2",
+        trainable=trainable)
+    outputs = encoder(encoder_inputs)
+    pooled_output = outputs["pooled_output"]      
+    sequence_output = outputs["sequence_output"]  
+    embedding_model = tf.keras.Model(text_input, pooled_output)
+    return embedding_model
 		
 		
 if __name__=='__main__':
