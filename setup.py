ca6e33297bedc876f047d7b065e7fdbcb2ff01a7
tikendraw <tikendraksahu1029@gmail.com>
2023-09-15 13:16:24 +0530

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='funcyou',
-    version='1.0.9',
+    version='1.1.0',
     author='Tikendra sahu',
     author_email='tikendraksahu1029@gmail.com',
-    description='useful function to save time',
+    description='useful functions to reuse and save time',
     packages=find_packages(),
     install_requires=[
         'matplotlib',
